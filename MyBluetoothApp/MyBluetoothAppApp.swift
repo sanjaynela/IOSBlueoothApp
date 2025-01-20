9d025ead1feff422c4f07f8cd72a9d9ff932d19e
sanjaynela <ballacksanjay@hotmail.com>
2025-01-20 07:04:21 -0800

    Initial Commit

---
+++ MyBluetoothApp/MyBluetoothAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBluetoothAppApp.swift
+//  MyBluetoothApp
+//
+//  Created by Sanjay Nelagadde on 20/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBluetoothAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBluetoothAppTests/MyBluetoothAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBluetoothAppTests.swift
+//  MyBluetoothAppTests
+//
+//  Created by Sanjay Nelagadde on 20/1/25.
+//
+
+import Testing
+@testable import MyBluetoothApp
+
+struct MyBluetoothAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
